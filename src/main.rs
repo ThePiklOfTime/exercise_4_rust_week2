fc569f2cd01610e51ecdcdf5aeb9b461dbae4027
ThePiklOfTime <nicotolvanen@gmail.com>
2026-06-04 18:25:22 +0300

    Made it work

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+mod map;
+use map::arraymap;
+fn main() {
+    let mut map = arraymap::create_map();
+    loop {
+        arraymap::print_map(&mut map);
+        println!("| w) Move up | a) Move left | s) Move down | d) Move right | e) End program |");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).expect("Failed to read line");
+        let direction = input.trim();
+        if direction == "e" {
+            break;
+        }
+        arraymap::move_player(&mut map, direction);
+    }
+
+
+}
---
+++ src/map/arraymap.rs
@@ -0,0 +1,70 @@
+
+pub fn print_map (map: &mut [[String; 5]; 5]) {
+    for row in map.iter() {
+        for cell in row.iter() {
+            print!("{} ", cell);
+        }
+        println!();
+    }
+
+}
+pub fn create_map() -> [[String; 5]; 5] {
+    let map = [
+        [String::from("*"), String::from("*"), String::from("*"), String::from("*"), String::from("*")],
+        [String::from("*"), String::from("*"), String::from("*"), String::from("*"), String::from("*")],
+        [String::from("*"), String::from("*"), String::from("x"), String::from("*"), String::from("*")],
+        [String::from("*"), String::from("*"), String::from("*"), String::from("*"), String::from("*")],
+        [String::from("*"), String::from("*"), String::from("*"), String::from("*"), String::from("*")]
+    ];
+    map
+}
+pub fn move_player(map: &mut [[String; 5]; 5], direction: &str) {
+    let mut player_x = 0;
+    let mut player_y = 0;
+
+    for (y, row) in map.iter().enumerate() {
+        for (x, cell) in row.iter().enumerate() {
+            if *cell == "x" {
+                player_x = x;
+                player_y = y;
+            }
+        }
+    }
+    
+    map[player_y][player_x] = String::from("*");
+    match direction {
+        "w" => {
+            if player_y > 0 {
+                player_y -= 1;
+            }
+            else {
+                println!("Can't move out of the map")
+            }
+        },
+        "s" => {
+            if player_y < map.len() - 1 {
+                player_y += 1;
+            }
+            else {
+                println!("Can't move out of the map")
+            }
+        },
+        "a" => {
+            if player_x > 0 {
+                player_x -= 1;
+            }
+            else {
+                println!("Can't move out of the map")
+            }   
+        },
+        "d" => {
+            if player_x < map[0].len() - 1 {
+                player_x += 1;
+            }else {
+                println!("Can't move out of the map")
+            }
+        },
+        _ => println!("Invalid direction! Use 'w', 's', 'a' or 'd   '.")
+    }
+    map[player_y][player_x] = String::from("x");
+}
